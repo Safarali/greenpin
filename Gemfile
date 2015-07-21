2c377993acdea0e05cddd629a7dccf72e25ffb50
Safarali <mr.safarali@gmail.com>
2015-07-20 19:55:39 -0700

    Added image upload with paperclip

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'jbuilder', '~> 2.0'
 gem "bootstrap-sass"
 gem 'devise', '~> 3.4.0'
 gem 'responders'
+gem 'paperclip', github: 'thoughtbot/paperclip'
+
 
 gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -20,15 +20,12 @@ class PinsController < ApplicationController
 
   def create
     @pin = current_user.pins.build(pin_params)
-
-    
-      if @pin.save
-          redirect_to @pin, notice: 'Pin was successfully created.'
-          
-      else
-          render action: "new"
-      end
+    if @pin.save
+        redirect_to @pin, notice: 'Pin was successfully created.'    
+    else
+        render action: "new"
     end
+  end
   
 
  
@@ -59,6 +56,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
