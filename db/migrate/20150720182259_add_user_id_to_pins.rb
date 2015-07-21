fe5a50b0663725da8a6984709d97764862ad31cb
Safarali <mr.safarali@gmail.com>
2015-07-20 23:25:14 -0700

    Testing

--- db/migrate/20150720182259_add_user_id_to_pins.rb
+++ db/migrate/20150720182259_add_user_id_to_pins.rb
@@ -1,11 +1,9 @@
 class AddUserIdToPins < ActiveRecord::Migration
   def change
-    add_column :pins, :user_id, :integer
+    Pins.reset_column_information    
+add_column :pins, :user_id, :integer
+add_index :pins, :user_id
   end
 end
 
-def change
-Pins.reset_column_information    
-add_column :pins, :user_id, :integer
-add_index :pins, :user_id
-end
+
