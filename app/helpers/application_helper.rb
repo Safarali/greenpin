7e9a6476b142a5b90eb8d080953537c787be2a22
Safarali <mr.safarali@gmail.com>
2015-08-16 18:22:39 -0700

    Title added through Application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+#Returns full full title on a per-page basis.
+	def full_title(page_title ='')
+		base_title="Greenpin"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
